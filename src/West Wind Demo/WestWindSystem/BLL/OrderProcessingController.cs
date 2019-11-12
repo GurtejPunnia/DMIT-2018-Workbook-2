5e11a70abe9baae3a7537a731d94cd3235df1749
GurtejPunnia <48194711+GurtejPunnia@users.noreply.github.com>
2019-11-12 11:46:59 -0700

    .

---
+++ src/West Wind Demo/WestWindSystem/BLL/OrderProcessingController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WestWindSystem.DAL;
+using WestWindSystem.DataModels;
+
+namespace WestWindSystem.BLL
+{
+   public class OrderProcessingController
+    {
+        #region Queries
+        public List<OutstandingOrder> LoadOrders(int supplierId)
+        {
+            throw new NotImplementedException();
+        }
+        public List<ShipperSelection> ListShippers()
+        {
+            using (var context = new WestWindContext())
+            {
+                var result = from shipper in context.Shippers
+                             select new ShipperSelection
+                             {
+                                 ShipperId = shipper.ShipperID,
+                                 Shipper = shipper.CompanyName
+                             };
+                return result.ToList();
+            }
+           
+        }
+        #endregion
+
+        #region Commands
+
+        public void ShipOrder(int orderId, ShippingDirections shipping, List<ProductShipment> products)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
