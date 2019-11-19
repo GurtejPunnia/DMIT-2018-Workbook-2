6327945a77b3de5cdfc0b7394af5d07a3eed9858
GurtejPunnia <48194711+GurtejPunnia@users.noreply.github.com>
2019-11-19 11:47:40 -0700

    .

--- src/West Wind Demo/WebApp/Sales/OrderShipping.aspx.cs
+++ src/West Wind Demo/WebApp/Sales/OrderShipping.aspx.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using WebApp.Admin.Security;
+using WestWindSystem.BLL;
+using WestWindSystem.DataModels;
 
 namespace WebApp.Sales
 {
@@ -22,6 +24,61 @@ namespace WebApp.Sales
                 // TODO: Replace hard-coded supplier ID with the user's supplier ID
                 // - Show Company name and contact
                 SupplierInfo.Text = "Temp supplier: ID 2";
+            }
+        }
+
+        protected void CurrentOrders_ItemCommand(object sender, ListViewCommandEventArgs e)
+        {
+            if(e.CommandName == "Ship")
+            {
+                int orderId = 0;
+                ShippingDirections shippingInfo = new ShippingDirections();
+                Label idLabel = e.Item.FindControl("OrderIdLabel") as Label;
+                if(idLabel != null)
+                {
+                    orderId = int.Parse(idLabel.Text);
+                }
+                DropDownList shipVia = e.Item.FindControl("ShipperDropDown") as DropDownList;
+                if (shipVia != null)
+                {
+                    shippingInfo.ShipperId = int.Parse(shipVia.SelectedValue);
+                }
+
+                TextBox tracking = e.Item.FindControl("TrackingCode") as TextBox;
+                if (tracking != null)
+
+                    shippingInfo.TrackingCode = tracking.Text;
+
+                TextBox freight = e.Item.FindControl("FreightCharge") as TextBox;
+                decimal charge;
+                if (freight != null && decimal.TryParse(freight.Text, out charge))        
+                    shippingInfo.FreightCharge = charge;
+                // Extract the items being shipped, as per the gridview
+                List<ProductShipment> itemsShipped = new List<ProductShipment>();
+                GridView gv = e.Item.FindControl("ProductsGridView") as GridView;
+                if( gv != null)
+                {
+                    foreach( GridViewRow row in gv.Rows)
+                    {
+                        HiddenField proHidden = row.FindControl("ProdId") as HiddenField;
+                        TextBox shipqty = row.FindControl("ShipQuantity") as TextBox;
+                        if (proHidden != null && shipqty != null)
+                        {
+                            int qty;
+                            if(int.TryParse(shipqty.Text, out qty))
+                            {
+                                var item = new ProductShipment
+                                {
+                                    ProductId = int.Parse(proHidden.Value),
+                                    ShipQuantity = qty
+                                };
+                                itemsShipped.Add(item);
+                            }
+                        }
+                    }
+                }
+                var controller = new OrderProcessingController();
+                controller.ShipOrder(orderId, shippingInfo, itemsShipped);
             }
         }
     }
